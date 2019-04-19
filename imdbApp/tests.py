a1c0b379755fa9e074067fa0d5aac09525a77e34
Emre Baş <emrebas2@ogr.cbu.edu.tr>
2019-04-19 14:55:33 +0300

    test codes added for post request to API status

--- imdbApp/tests.py
+++ imdbApp/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
 
-# Create your tests here.
+
+class test_movie(APITestCase):
+    def setUp(self):
+        self.data={"title":"Oslo 31"}
+
+    def test_worksAPIorNot(self):
+        client = APIClient()
+        response=client.post('movietitle/', {'title':'oslo 31'}, format='json')
+        print(response.status_code)
+        assert response.status_code==200
