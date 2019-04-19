6b3cb595446c78d26d8f34c2c1d02f658a899a06
Emre Baş <emrebas2@ogr.cbu.edu.tr>
2019-04-19 15:20:27 +0300

    code refactor and title test code added

--- imdbApp/tests.py
+++ imdbApp/tests.py
@@ -1,12 +1,16 @@
+import json
 from rest_framework.test import APITestCase, APIClient
 
 
 class test_movie(APITestCase):
     def setUp(self):
+        self.client = APIClient()
         self.data={"title":"Oslo 31"}
+        self.response = self.client.post('movietitle/', {'title': 'oslo 31'}, format='json')
 
     def test_worksAPIorNot(self):
-        client = APIClient()
-        response=client.post('movietitle/', {'title':'oslo 31'}, format='json')
-        print(response.status_code)
-        assert response.status_code==200
+        print(self.response.status_code)
+        assert self.response.status_code==200
+
+    def test_movieTitle(self):
+        self.assertTrue("Oslo, August 31st" in json.loads(self.response.content))
